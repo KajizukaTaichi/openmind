f20caa14998159cecf6e9f5242369a8c73f0682b
梶塚太智 <kajizukataichi@outlook.jp>
2024-09-25 22:55:25 +0900

    対話環境を実装

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,60 @@
 use regex::Regex;
+use rustyline::DefaultEditor;
 use std::{
     collections::HashMap,
     env::args,
     fs::{read_to_string, File},
     io::Write,
+    process::exit,
 };
 
 fn main() {
+    let mut openmind = Core {
+        stack: vec![],
+        memory: HashMap::from([
+            ("改行".to_string(), Type::String("\n".to_string())),
+            ("空白".to_string(), Type::String(" ".to_string())),
+            ("十".to_string(), Type::Number(10.0)),
+            ("百".to_string(), Type::Number(100.0)),
+            ("千".to_string(), Type::Number(1000.0)),
+        ]),
+        cache: Type::Null,
+    };
+
     let args = args().collect::<Vec<String>>();
     if let Some(path) = args.get(1) {
         if let Ok(code) = read_to_string(path) {
-            let mut openmind = Core {
-                stack: vec![],
-                memory: HashMap::from([
-                    ("改行".to_string(), Type::String("\n".to_string())),
-                    ("十".to_string(), Type::Number(10.0)),
-                    ("百".to_string(), Type::Number(100.0)),
-                    ("千".to_string(), Type::Number(1000.0)),
-                    ("万".to_string(), Type::Number(1_0000.0)),
-                    ("億".to_string(), Type::Number(1_0000_0000.0)),
-                ]),
-                cache: Type::Null,
-            };
             openmind.eval(code);
         } else {
             eprintln!("エラー！ファイルが開けませんでした");
         };
     } else {
-        println!("日本語プログラミング言語OpenMind");
+        println!("日本語プログラミング言語 OpenMind");
+        let mut rl = DefaultEditor::new().unwrap();
+
+        loop {
+            let mut code = String::new();
+            loop {
+                let enter = rl.readline("> ").unwrap_or_default().trim().to_string();
+                if enter.is_empty() {
+                    break;
+                }
+                code += &format!("{enter} ");
+            }
+
+            if !code.is_empty() {
+                openmind.eval(code);
+                println!(
+                    "スタック〔 {} 〕",
+                    &openmind
+                        .stack
+                        .iter()
+                        .map(|i| i.get_symbol())
+                        .collect::<Vec<String>>()
+                        .join(" | ")
+                );
+            }
+        }
     }
 }
 
@@ -62,6 +89,15 @@ impl Type {
             Type::String(s) | Type::Function(s) => s.to_owned(),
             Type::Bool(b) => if *b { "真" } else { "偽" }.to_string(),
             Type::Null => String::new(),
+        }
+    }
+
+    fn get_symbol(&self) -> String {
+        match self {
+            Type::Number(i) => i.to_string(),
+            Type::String(s) | Type::Function(s) => format!("「{}」", s),
+            Type::Bool(b) => if *b { "真" } else { "偽" }.to_string(),
+            Type::Null => "無".to_string(),
         }
     }
 
@@ -178,86 +214,92 @@ impl Core {
             } else {
                 match token.as_str() {
                     "表示" => {
-                        println!("{}", self.stack.pop().unwrap().get_string());
+                        println!("{}", self.pop().get_string());
+                    }
+                    "入力待" => {
+                        let prompt = &self.pop().get_string();
+                        self.stack.push(Type::String(
+                            DefaultEditor::new().unwrap().readline(prompt).unwrap(),
+                        ))
                     }
                     "結合" => {
-                        let str2 = self.stack.pop().unwrap().get_string();
-                        let str1 = self.stack.pop().unwrap().get_string();
+                        let str2 = self.pop().get_string();
+                        let str1 = self.pop().get_string();
                         self.stack.push(Type::String(str1 + &str2));
                     }
                     "足" => {
-                        let num2 = self.stack.pop().unwrap().get_number();
-                        let num1 = self.stack.pop().unwrap().get_number();
+                        let num2 = self.pop().get_number();
+                        let num1 = self.pop().get_number();
                         self.stack.push(Type::Number(num1 + num2));
                     }
                     "引" => {
-                        let num2 = self.stack.pop().unwrap().get_number();
-                        let num1 = self.stack.pop().unwrap().get_number();
+                        let num2 = self.pop().get_number();
+                        let num1 = self.pop().get_number();
                         self.stack.push(Type::Number(num1 - num2));
                     }
                     "掛" => {
-                        let num2 = self.stack.pop().unwrap().get_number();
-                        let num1 = self.stack.pop().unwrap().get_number();
+                        let num2 = self.pop().get_number();
+                        let num1 = self.pop().get_number();
                         self.stack.push(Type::Number(num1 * num2));
                     }
                     "割" => {
-                        let num2 = self.stack.pop().unwrap().get_number();
-                        let num1 = self.stack.pop().unwrap().get_number();
+                        let num2 = self.pop().get_number();
+                        let num1 = self.pop().get_number();
                         self.stack.push(Type::Number(num1 / num2));
                     }
                     "余" => {
-                        let num2 = self.stack.pop().unwrap().get_number();
-                        let num1 = self.stack.pop().unwrap().get_number();
+                        let num2 = self.pop().get_number();
+                        let num1 = self.pop().get_number();
                         self.stack.push(Type::Number(num1 % num2));
                     }
                     "等" => {
-                        let str1 = self.stack.pop().unwrap().get_string();
-                        let str2 = self.stack.pop().unwrap().get_string();
+                        let str1 = self.pop().get_symbol();
+                        let str2 = self.pop().get_symbol();
                         self.stack.push(Type::Bool(str1 == str2));
                     }
                     "大" => {
-                        let num2 = self.stack.pop().unwrap().get_number();
-                        let num1 = self.stack.pop().unwrap().get_number();
+                        let num2 = self.pop().get_number();
+                        let num1 = self.pop().get_number();
                         self.stack.push(Type::Bool(num1 > num2));
                     }
                     "小" => {
-                        let num2 = self.stack.pop().unwrap().get_number();
-                        let num1 = self.stack.pop().unwrap().get_number();
+                        let num2 = self.pop().get_number();
+                        let num1 = self.pop().get_number();
                         self.stack.push(Type::Bool(num1 < num2));
                     }
                     "和" => {
-                        let bool2 = self.stack.pop().unwrap().get_bool();
-                        let bool1 = self.stack.pop().unwrap().get_bool();
+                        let bool2 = self.pop().get_bool();
+                        let bool1 = self.pop().get_bool();
                         self.stack.push(Type::Bool(bool1 || bool2));
                     }
                     "積" => {
-                        let bool2 = self.stack.pop().unwrap().get_bool();
-                        let bool1 = self.stack.pop().unwrap().get_bool();
+                        let bool2 = self.pop().get_bool();
+                        let bool1 = self.pop().get_bool();
                         self.stack.push(Type::Bool(bool1 && bool2));
                     }
                     "否" => {
-                        let bool1 = self.stack.pop().unwrap().get_bool();
+                        let bool1 = self.pop().get_bool();
                         self.stack.push(Type::Bool(!bool1));
                     }
                     "代入" => {
-                        let name = self.stack.pop().unwrap().get_string();
-                        let value = self.stack.pop().unwrap();
+                        let name = self.pop().get_string();
+                        let value = self.pop();
                         self.cache = value.clone();
                         self.memory.insert(name, value);
                     }
                     "定義" => {
-                        let name = self.stack.pop().unwrap().get_string();
-                        let code = self.stack.pop().unwrap().get_string();
+                        let name = self.pop().get_string();
+                        let code = self.pop().get_string();
                         self.memory.insert(name, Type::Function(code));
                     }
                     "評価" => {
-                        let code = self.stack.pop().unwrap().get_string();
+                        let code = self.pop().get_string();
                         self.eval(code)
                     }
                     "条件分岐" => {
-                        let code_false = self.stack.pop().unwrap().get_string();
-                        let code_true = self.stack.pop().unwrap().get_string();
-                        let condition = self.stack.pop().unwrap().get_bool();
+                        let code_false = self.pop().get_string();
+                        let code_true = self.pop().get_string();
+                        let condition = self.pop().get_bool();
                         if condition {
                             self.eval(code_true)
                         } else {
@@ -265,28 +307,37 @@ impl Core {
                         }
                     }
                     "反復" => {
-                        let code = self.stack.pop().unwrap().get_string();
-                        let condition = self.stack.pop().unwrap().get_string();
+                        let code = self.pop().get_string();
+                        let condition = self.pop().get_string();
                         while {
                             self.eval(condition.clone());
-                            self.stack.pop().unwrap().get_bool()
+                            self.pop().get_bool()
                         } {
                             self.eval(code.clone());
                         }
                     }
                     "読" => {
-                        let path = self.stack.pop().unwrap().get_string();
+                        let path = self.pop().get_string();
                         self.stack
                             .push(Type::String(read_to_string(path).unwrap_or(String::new())));
                     }
                     "書" => {
-                        let path = self.stack.pop().unwrap().get_string();
-                        let value = self.stack.pop().unwrap().get_string();
+                        let path = self.pop().get_string();
+                        let value = self.pop().get_string();
                         File::create(path).unwrap().write(value.as_bytes()).unwrap();
                     }
+                    "終了" => exit(0),
                     other => self.stack.push(Type::String(other.to_string())),
                 }
             }
+        }
+    }
+
+    fn pop(&mut self) -> Type {
+        if let Some(value) = self.stack.pop() {
+            value
+        } else {
+            Type::Null
         }
     }
 }
